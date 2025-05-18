fe856451e81dcaba9ab1c3d850fa1398edccdd2e
Aryan <aryankansal15@gmail.com>
2025-05-18 18:11:39 +0530

    day 8 CF contest Q1

--- Day-8/2109A.cpp
+++ Day-8/2109A.cpp
@@ -1,45 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int t;
-    cin >> t;
-    
-    while (t--) {
-        int n;
-        cin >> n;
-        
-        vector<int> reports(n);
-        for (int i = 0; i < n; i++) {
-            cin >> reports[i];
+ 
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<int> v;
+        for(int i=0; i<n; i++){
+            int x; cin>>x;
+            v.push_back(x);
         }
-        
-        // Count winners and losers from reports
-        int winners = 0;
-        for (int i = 0; i < n; i++) {
-            if (reports[i] == 1) {
-                winners++;
+ 
+        int c1 = 0, c2 = 1;
+        for(int i=0 ; i<n-1; i++){
+            if(v[i]==0){
+                c1 = 1;
+                if(v[i+1] == 0){
+                    c2 = 0;
+                }
             }
         }
-        
-        // In n-1 matches, there must be exactly n-1 losses and n-1 wins total
-        // So there must be at least n-1 players reporting a win (value 1)
-        // Or, at most 1 player can report no wins (value 0)
-        
-        int zeros = n - winners;
-        
-        // If there are more than 1 player reporting 0, someone must be lying
-        if (zeros > 1) {
-            cout << "YES" << endl; // There's a liar
-        } else if (zeros == 0) {
-            // If everyone reports winning at least one match
-            // This is impossible since n-1 matches means at least one person must have lost all matches
-            cout << "YES" << endl; // There's a liar
-        } else {
-            // Exactly one player reports 0 - this could be consistent
-            cout << "NO" << endl; // No proven liar
+        if(v[n-1]==0){
+            c1=1;
+        }
+ 
+        if(c1 && c2){
+            cout<<"NO"<<endl;
+        } else{
+            cout<<"YES"<<endl;
         }
     }
-    
-    return 0;
 }
